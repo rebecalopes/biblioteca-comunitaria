88509a8e4e955c92acbdef99aa3af0a98bdbd5ef
rebecalopes <rebeca.lopes.141@gmail.com>
2021-11-04 23:41:23 -0300

    adicionando primeiros controllers e views

---
+++ MvcMovie/Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult IndexAdmin()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroLivro()
+        {
+            return View();
+        }
+        public IActionResult Emprestimo()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Arcevo()
+        {
+            return View();
+        }
+        public IActionResult Cursos()
+        {
+            return View();
+        }
+
+        public IActionResult SobreNos()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
